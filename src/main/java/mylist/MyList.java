ab8c83fd6f81211f85b5ed4a1e00c1bdee9d06d2
Kadomtsev Kirill <kotoforma@gmail.com>
2026-03-29 21:26:59 +0300

    fix: package name

---
+++ src/main/java/mylist/MyList.java
@@ -0,0 +1,64 @@
+package mylist;
+
+public class MyList {
+    
+    public MyList() {
+    }
+    
+    public MyList(int capacity) {
+    }
+    
+    public MyList(int[] array) {
+    }
+    
+    public int get(int index) {
+        return -1;
+    }
+    
+    public void set(int index, int value) {
+    }
+    
+    public void add(int value) {
+    }
+    
+    public void add(int index, int value) {
+    }
+    
+    public void remove(int index) {
+    }
+    
+    public int pop() {
+        return -1;
+    }
+    
+    public int indexOf(int value) {
+        return -1;
+    }
+    
+    public boolean contains(int value) {
+        return false;
+    }
+    
+    public int size() {
+        return 0;
+    }
+    
+    public boolean isEmpty() {
+        return false;
+    }
+    
+    public int[] toArray() {
+        return new int[0];
+    }
+    
+    public void clear() {
+    }
+    
+    public boolean equals(Object o) {
+        return false;
+    }
+    
+    public int hashCode() {
+        return 0;
+    }
+}
